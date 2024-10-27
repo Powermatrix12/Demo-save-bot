afbb1abda6d03a58607112b3ecadc5967a30720a
Powermatrix12 <167418825+Powermatrix12@users.noreply.github.com>
2024-10-27 23:07:32 +0530

    Create strings.py

---
+++ TechVJ/strings.py
@@ -0,0 +1,25 @@
+# Don't Remove Credit Tg - @VJ_Botz
+# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
+# Ask Doubt on telegram @KingVJ01
+
+strings = {
+    'need_login': "U have to /login before using then bot can download restricted content ❕",
+    'already_logged_in': "You are already logged in.\nIf you want to login again, /logout to proceed.",
+    'broadcast_started': "Broadcasting message to all users...",
+    'broadcast_completed': "Broadcast completed.",
+    'user_count': "Total users: {count}",
+    'user_list': "User List:\n{users}"
+}
+
+HELP_TXT = """**🌟 Help Menu** 
+
+**__FOR PRIVATE CHATS__**
+
+__first send invite link of the chat (unnecessary if the account of string session already member of the chat)
+then send post/s link__
+
+
+**__FOR BOT CHATS__**
+
+__send link with '/b/', bot's username and message id, you might want to install some unofficial client to get the id like below__
+
