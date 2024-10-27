0ee33a76698f9818bea35a94fb769f2f0b0efebc
Powermatrix12 <167418825+Powermatrix12@users.noreply.github.com>
2024-10-27 23:10:01 +0530

    Update strings.py

--- TechVJ/strings.py
+++ TechVJ/strings.py
@@ -22,4 +22,11 @@ then send post/s link__
 **__FOR BOT CHATS__**
 
 __send link with '/b/', bot's username and message id, you might want to install some unofficial client to get the id like below__
+https://t.me/b/botusername/4321
 
+**__MULTI POSTS__**
+
+__send public/private posts link as explained above with formate "from - to" to send multiple messages like below__
+
+https://t.me/xxxx/1001-1010
+https://t.me/c/xxxx/101
